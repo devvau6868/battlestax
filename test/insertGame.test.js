a380e89ac525192dad2ed57f0659cadce64d4161
sonicfmg <david.gilardi@datastax.com>
2021-02-19 12:46:44 -0500

    Added extended timeout to insertGame test

--- test/insertGame.test.js
+++ test/insertGame.test.js
@@ -9,7 +9,7 @@ it.skip("should create a game document", async () => {
     body: '{"user":"me"}',
   });
   expect(response.statusCode).toBe(200);
-});
+}, 30000);
 
 it.skip("shouldn't create a game document without a game id", async () => {
   const response = await insertGame.handler({ path: "insertGame" });
